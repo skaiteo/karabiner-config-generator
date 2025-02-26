1a113c5e0c238b1d229a6d90c10166e85d0f7dc8
SHENTEO <sheng_kai.teo@mercedes-benz.com>
2025-02-26 14:36:41 +0800

    add Option+F => Ctrl+F

--- src/charlie.js
+++ src/charlie.js
@@ -56,6 +56,62 @@ const charlie = [
     ],
   },
   {
+    description: "Option+F => Ctrl+F",
+    type: "basic",
+    from: {
+      key_code: "f",
+      modifiers: {
+        mandatory: ["option"],
+        optional: ["any"],
+      },
+    },
+    to: [
+      {
+        key_code: "f",
+        modifiers: ["left_control"],
+      },
+    ],
+    conditions: [
+      {
+        type: "frontmost_application_unless",
+        bundle_identifiers: [
+          "^com\\.utmapp\\.UTM$",
+          "^com\\.microsoft\\.rdc$",
+          "^com\\.microsoft\\.rdc\\.mac$",
+          "^com\\.microsoft\\.rdc\\.macos$",
+          "^com\\.microsoft\\.rdc\\.osx\\.beta$",
+          "^net\\.sf\\.cord$",
+          "^com\\.thinomenon\\.RemoteDesktopConnection$",
+          "^com\\.itap-mobile\\.qmote$",
+          "^com\\.nulana\\.remotixmac$",
+          "^com\\.p5sys\\.jump\\.mac\\.viewer$",
+          "^com\\.p5sys\\.jump\\.mac\\.viewer\\.web$",
+          "^com\\.teamviewer\\.TeamViewer$",
+          "^com\\.vmware\\.horizon$",
+          "^com\\.2X\\.Client\\.Mac$",
+          "^com\\.vmware\\.fusion$",
+          "^com\\.vmware\\.horizon$",
+          "^com\\.vmware\\.view$",
+          "^com\\.parallels\\.desktop$",
+          "^com\\.parallels\\.vm$",
+          "^com\\.parallels\\.desktop\\.console$",
+          "^org\\.virtualbox\\.app\\.VirtualBoxVM$",
+          "^com\\.citrix\\.XenAppViewer$",
+          "^com\\.vmware\\.proxyApp\\.",
+          "^com\\.parallels\\.winapp\\.",
+          "^org\\.macports\\.X11$",
+          "^com\\.apple\\.Terminal$",
+          // "^com\\.googlecode\\.iterm2$",
+          "^co\\.zeit\\.hyperterm$",
+          "^co\\.zeit\\.hyper$",
+          "^io\\.alacritty$",
+          "^net\\.kovidgoyal\\.kitty$",
+          "^tv\\.parsec\\.www$",
+        ],
+      },
+    ],
+  },
+  {
     description: "Ctrl+Backspace => Alt+Backspace",
     type: "basic",
     from: {
@@ -108,58 +164,6 @@ const charlie = [
           "^net\\.kovidgoyal\\.kitty$",
           "^tv\\.parsec\\.www$",
         ],
-      },
-    ],
-  },
-  {
-    description: "F1,F2,F3,F4 => Ctrl+Arrows",
-    type: "basic",
-    from: {
-      key_code: "f1",
-    },
-    to: [
-      {
-        key_code: "left_arrow",
-        modifiers: ["left_control"],
-      },
-    ],
-  },
-  {
-    description: "F1,F2,F3,F4 => Ctrl+Arrows",
-    type: "basic",
-    from: {
-      key_code: "f2",
-    },
-    to: [
-      {
-        key_code: "right_arrow",
-        modifiers: ["left_control"],
-      },
-    ],
-  },
-  {
-    description: "F1,F2,F3,F4 => Ctrl+Arrows",
-    type: "basic",
-    from: {
-      key_code: "f3",
-    },
-    to: [
-      {
-        key_code: "up_arrow",
-        modifiers: ["left_control"],
-      },
-    ],
-  },
-  {
-    description: "F1,F2,F3,F4 => Ctrl+Arrows",
-    type: "basic",
-    from: {
-      key_code: "f4",
-    },
-    to: [
-      {
-        key_code: "down_arrow",
-        modifiers: ["left_control"],
       },
     ],
   },
